4b34682da3f270f05d1bbc0e18ed49f79d29f346
hyunhoon <khj2253@gmail.com>
2026-06-29 13:27:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+send_telegram("테스트 메시지입니다. 봇이 정상 작동 중입니다.")
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 import os
 import json # 알림 기록용
 import requests # 텔레그램 전송용
-send_telegram("테스트 메시지입니다. 봇이 정상 작동 중입니다.")
 
 # 텔레그램 설정 (이 부분만 본인의 값으로 바꾸세요)
 TELEGRAM_TOKEN = "8738343974:AAFrFB26q547kfnj9-xRwHnyVj1qRs0KdlI"
